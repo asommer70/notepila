04e1810f03514717db494abdbf0ae3e6d9cd7e9d
Adam Sommer <adam@thehoick.com>
2017-01-27 23:06:17 -0500

    Start working on tests.

---
+++ test/models/folders_test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe('Folders', function() {
+
+  describe('index', function() {
+    it('should return a list of folders', function(done) {
+      
+    });
+  });
+})
